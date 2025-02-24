9b3acd94c8a6aabc7f74999fe63699c3a61c7585
Jeremy Nelson <jnelson@epicsol.org>
2025-02-24 15:43:24 -0600

    (3020): (<- 2156) Fix/rewrite $uname() [rb sob]

--- source/irc.c
+++ source/irc.c
@@ -52,7 +52,7 @@ const char internal_version[] = "20240826";
 /*
  * In theory, this number is incremented for every commit.
  */
-const unsigned long	commit_id = 3019;
+const unsigned long	commit_id = 3020;
 
 /*
  * As a way to poke fun at the current rage of naming releases after
